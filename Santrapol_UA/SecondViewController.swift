3a6998eac391fbba33549254b20fefe4749891ee
Tamanna Manocha <tamanna@tamannas-macbook-air.local>
2019-02-06 20:19:23 -0500

    Initial Commit

---
+++ Santrapol_UA/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Santrapol_UA
+//
+//  Created by Tamanna Manocha on 2019-02-06.
+//  Copyright © 2019 Anshul Manocha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
